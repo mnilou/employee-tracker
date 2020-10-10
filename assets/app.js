f3b39c9bcb8a468423a389c051fd8e93655ea2d9
Nilou Mostofi <nilou@km-direct.com>
2020-10-10 14:44:59 -0700

    updated db

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,78 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const {printTable} = require("console.table");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const {printTable} = require('console.table');
 const figlet = require('figlet');
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
 
   // Your port; if not 3306
   port: 3306,
 
   // Your username
-  user: "root",
+  user: 'root',
 
   // Your password
-  password: "password",
-  database: "employee_trackerDB",
+  password: 'password',
+  database: 'employee_trackerDB',
+});
+
+figlet('Employee Tracker', (err, result) => {
+  console.log(err || result);
 });
 
 connection.connect((err) => {
   if (err) {
     throw err;
   }
-  console.log("connected as id " + connection.threadId + "\n");
-//   createProduct();
+  console.log('connected as id ' + connection.threadId + '\n');
+  startApp();
 });
+startApp = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'choice',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View All Employees',
+          'View All Departments',
+          'View All Roles',
+          'Add Employees',
+          'Add Departments',
+          'Add Roles',
+          'Update Employee Role',
+          'EXIT',
+        ],
+      },
+    ])
+    .then(function(answer) {
+      switch (answer.choice) {
+        case "View All Employees":
+          empAllSearch();
+          break;
+        case "View All Departments":
+          deptSearch();
+          break;
+        case "View All Roles":
+          roleSearch();
+          break;
+        case "Add Employees":
+          addEmp();
+          break;
+        case "Add Departments":
+          addDept();
+          break;
+        case "Add Roles":
+          addRole();
+          break;
+        case "Update Employee Roles":
+          updateEmpRole();
+          break;
+        case "EXIT":
+          connection.end();
+          break;
+      }
+    });
+};
--- assets/schema.sql
+++ assets/schema.sql
@@ -6,16 +6,16 @@ USE employee_trackerDB;
 
 CREATE TABLE department
 (
-  id INT NOT NULL
+  department_id INT NOT NULL
   AUTO_INCREMENT,
 name VARCHAR
   (30) NOT NULL,
 PRIMARY KEY
-  (id)
+  (department_id)
 );
   CREATE TABLE role
   (
-    id INT NOT NULL
+    role_id INT
     AUTO_INCREMENT,
 title VARCHAR
     (30) NOT NULL,
@@ -23,28 +23,37 @@ salary DECIMAL
     (10,2) NOT NULL,
 department_id INT DEFAULT 1,
 PRIMARY KEY
-    (id),
-FOREIGN KEY
+    (role_id),
+-- FOREIGN KEY(department_id) REFERENCES department(id), 
+CONSTRAINT fk_department FOREIGN KEY
     (department_id) REFERENCES department
-    (id) 
-);
+    (department_id) ON
+    DELETE
+    SET NULL
+    );
     CREATE TABLE employee
     (
-      id INT NOT NULL
+      employee_id INT
       AUTO_INCREMENT,
 first_name VARCHAR
       (30) NOT NULL,
 last_name VARCHAR
       (30) NOT NULL,
-role_id INT NOT NULL, 
+role_id INT, 
 department_id INT DEFAULT 1,
-manager_id INT  NOT NULL,
+manager_id INT NOT NULL,
 PRIMARY KEY
-      (id),
-FOREIGN KEY
-      (department_id) REFERENCES department
-      (id),
-FOREIGN KEY
+      (employee_id),
+-- FOREIGN KEY(department_id) REFERENCES department(id),
+-- FOREIGN KEY (manager_id) REFERENCES employee(id),
+CONSTRAINT fk_role FOREIGN KEY
+      (role_id) REFERENCES role
+      (role_id) ON
+      DELETE
+      SET NULL
+      ,
+CONSTRAINT fk_manager FOREIGN KEY
       (manager_id) REFERENCES employee
-      (id)
+      (employee_id) ON
+      DELETE CASCADE
 );
